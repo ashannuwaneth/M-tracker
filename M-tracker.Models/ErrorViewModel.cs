c358fd59066dcb3201b97456d93d55a9eee9e816
ashannuwaneth <ashannuwaneth077@gmail.com>
2022-03-27 12:30:10 +1100

    setup according to the N-tire

--- M-tracker.Models/ErrorViewModel.cs
+++ M-tracker.Models/ErrorViewModel.cs
@@ -1,3 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace M_tracker.Models
 {
     public class ErrorViewModel
