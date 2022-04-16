deb9fd720da3b21f1a6282fd4519ac246f3a35bc
ashannuwaneth <ashannuwaneth077@gmail.com>
2022-04-17 02:17:31 +1000

    expenses manage updated

--- M-tracker/wwwroot/js/GroupExpenses.js
+++ M-tracker/wwwroot/js/GroupExpenses.js
@@ -1,45 +1,99 @@
 ﻿
-
 var dataTable;
 
 $(document).ready(function () {
 
-    $('#datepicker').datetimepicker({
+    $('#txtDateFrom').datepicker();
+    $('#txtDateTo').datepicker();
 
-        defaultDate: new Date(),
-        format: 'DD/MM/YYYY'
+    InitDataTable();
+    InitDisableItems();
 
-    });
+    $("#txtDateTo").change(EnableFields);
+    $('#drpGroup').change(Dropdiable);
+    $('#AddProdBtn').click(AddToGrid);
+    $('#ClearBtn').click(ClearForm);
 
- 
+
+
+
+
+});
+
+var MainArr = [];
+
+function Dropdiable() {
+
+    $('#drpGroup').attr('disabled', true);
+
+}
+
+function InitDisableItems() {
+
+    $('#drpGroup').attr('disabled', true);
+    $('#drpExpenses').attr('disabled', true);
+    $('#txtDescription').attr('disabled', true);
+    $('#txtamount').attr('disabled', true);
+}
+
+function InitDataTable() {
+
     dataTable = $('#tblGroup').DataTable({
         responsive: true,
         "columns": [
-            { "data": "amount", "width": "20%" },
-            { "data": "description", "width": "20%" }
+            { "data": "group", "width": "20%" },
+            { "data": "type", "width": "20%" },
+            { "data": "description", "width": "20%" },
+            { "data": "amount", "width": "20%" }
         ],
-
     });
+}
+
+function EnableFields() {
+
+    $("#drpGroup").removeAttr("disabled");
+    $("#drpExpenses").removeAttr("disabled");
+    $("#txtDescription").removeAttr("disabled");
+    $("#txtamount").removeAttr("disabled");
+
+
+    $("#TxtFrom").attr("disabled", true);
+    $("#TxtTo").attr("disabled", true);
+
+}
+
+function AddToGrid() {
+
+    var TempArr = {};
+    TempArr["group"] = $('#drpGroup :selected').text();
+    TempArr["type"] = $('#drpExpenses :selected').text();
+    TempArr["description"] = $('#txtDescription').val() == "" ? $('#drpExpenses :selected').text() : $('#txtDescription').val();
+    TempArr["amount"] = $('#txtamount').val();
+
+    MainArr.push(TempArr);
+    dataTable.clear().draw();
 
 
 
+    for (var i = 0; i < MainArr.length; i++) {
+        dataTable.row.add(MainArr[i]).draw(false);
+    }
 
-    $('#AddProdBtn').click(function () {
+    var TotalAmount=0;
 
-        var Mainarr = [];
-        var arr = {};
-        arr["amount"] = $('#txtamount').val();
-        arr["description"] = $('#txtamount').val();
+    for (var j = 0; j < MainArr.length; j++) {
 
-        Mainarr.push(arr);
+        var amount = MainArr[j]["amount"];
 
-        for (var i = 0; i < Mainarr.length; i++) {
-            dataTable.row.add(Mainarr[i]).draw(false);
-        }
-
-
-    });
-
-})
+        TotalAmount = parseFloat(amount) + parseFloat(TotalAmount);
+    }
 
 
+    $('#txtalert').text(parseFloat(TotalAmount));
+
+}
+
+function ClearForm() {
+
+
+}
